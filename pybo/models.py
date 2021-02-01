6834840d044b6dfea108b8e42401a3ffe7ec7288
yekyung2 <skybluee2014@gmail.com>
2021-02-01 17:23:19 +0900

    add Question, Answer models and register admin

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.content
