b34203f1474836170eaaeb866dd3ca6c5f449b6c
yekyung2 <skybluee2014@gmail.com>
2021-02-01 19:14:51 +0900

    fix hardcoding in question_list.html

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 # from .models import Question
 
+app_name = "pybo"
+
 urlpatterns = [
-    path("", views.index),
-    path("<int:question_id>/", views.detail),
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 # Create your views here.
@@ -15,7 +15,7 @@ def index(request):
 def detail(request, question_id):
 
     """pybo 내용 출력"""
-    
-    question = Question.objects.get(id=question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
     context = {"question": question}
     return render(request, "pybo/question_detail.html", context)
